001bd89d533a7a553f0656677ed7be826dacb856
GitHub Action <action@github.com>
2021-06-06 17:44:58 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/scyzoryck/blackfireintellijplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.scyzoryck.blackfireintellijplugin.services
+
+import com.github.scyzoryck.blackfireintellijplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/scyzoryck/blackfireintellijplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/scyzoryck/blackfireintellijplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.scyzoryck.blackfireintellijplugin.services
 
+import com.github.scyzoryck.blackfireintellijplugin.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
